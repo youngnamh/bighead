08c2a265005edc6bd039f8253571b5b723166aea
youngnamh <youngnamhlady@gmail.com>
2024-03-23 18:59:39 -0400

    adding routing now

--- src/app/components/lp-main-content/lp-main-content.component.ts
+++ src/app/components/lp-main-content/lp-main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-lp-main-content',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, RouterModule],
   templateUrl: './lp-main-content.component.html',
   styleUrl: './lp-main-content.component.scss',
 })
