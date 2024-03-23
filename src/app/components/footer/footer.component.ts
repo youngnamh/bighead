b9e9bc18bdd9224bfb462b96af9952e97b576726
youngnamh <youngnamhlady@gmail.com>
2024-03-23 17:35:10 -0400

    footer working. responsive landing page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  currentYear!: number;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
 }
