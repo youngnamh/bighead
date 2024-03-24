4565f89847919af1c31ab01c4afc4296c3328728
youngnamh <youngnamhlady@gmail.com>
2024-03-23 23:33:40 -0400

    added redirect url for strava auth

--- src/app/components/lp-main-content/lp-main-content.component.ts
+++ src/app/components/lp-main-content/lp-main-content.component.ts
@@ -9,4 +9,17 @@ import { RouterModule } from '@angular/router';
   templateUrl: './lp-main-content.component.html',
   styleUrl: './lp-main-content.component.scss',
 })
-export class LpMainContentComponent {}
+export class LpMainContentComponent {
+  stravaAuth: string =
+    'http://www.strava.com/oauth/authorize?client_id=123612&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read';
+  clientId: string = '123612';
+  clientSecret: string = 'a5530b9a4d2dbbd1a80a13a3789c36cda48f8cb9';
+  code: string = '';
+  authUrl: string = `https://www.strava.com/api/v3/oauth/token?client_id=${this.clientId}&client_secret=${this.clientSecret}&code=${this.code}&grant_type=authorization_code`;
+
+  redirectToStrava() {
+    console.log('hello');
+    console.log(this.authUrl);
+    //window.open(this.stravaAuth, '_blank');
+  }
+}
